315ceb3d88290db22e5721f8cc573420dfeefa8d
jovalanaco <stefan.jovic@lanaco.com>
2024-02-16 09:40:37 +0100

    7. JSON modul

--- PocetakOsnove/webapp/controller/App.controller.js
+++ PocetakOsnove/webapp/controller/App.controller.js
@@ -1,10 +1,23 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     "use strict"
-    return Controller.extend("sap.ui.demo.walkthrough.",{
-        onShowHello : function () {
+    return Controller.extend("sap.ui.demo.walkthrough.App",{
+        
+            //JSON
+            onInit : function () {
+                var oData = {
+                    recipient : {
+                        name : "UI5"
+                    }
+                };
+                var oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+            },
+            onShowHello : function () {
+            //Native JS alert
             MessageToast.show("Hellou!")
         }
     });
