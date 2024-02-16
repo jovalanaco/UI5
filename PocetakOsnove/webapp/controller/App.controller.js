eace766e0bdddc6321a681a4c711075ef8bf0a0a
jovalanaco <stefan.jovic@lanaco.com>
2024-02-16 13:59:00 +0100

    9. Konfigurisanje komponenti/ Sredjivanje koda u komponente

--- PocetakOsnove/webapp/controller/App.controller.js
+++ PocetakOsnove/webapp/controller/App.controller.js
@@ -6,25 +6,6 @@ sap.ui.define([
 ], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
-        
-            //JSON
-            onInit : function () {
-                var oData = {
-                    recipient : {
-                        name : "UI5"
-                    }
-                };
-                var oModel = new JSONModel(oData);
-                this.getView().setModel(oModel);
-                //i18n model
-                var i18nModel = new ResourceModel({
-                    bundleName : "sap.ui.demo.walkthrough.i18n.i18n",
-                    supportedLocales : [""],
-                    fallbackLocale : ""
-                });
-                this.getView().setModel(i18nModel, "i18n")
-
-            },
             onShowHello : function () {
             //read message i18n
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
