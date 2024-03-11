232a39f450c15978c05f24e88e480a3e7b2725b0
jovalanaco <160107748+jovalanaco@users.noreply.github.com>
2024-03-11 12:37:57 +0100

    17. Opozivi za dijaloge

--- PocetakOsnove/webapp/controller/HelloPanel.controller.js
+++ PocetakOsnove/webapp/controller/HelloPanel.controller.js
@@ -19,7 +19,8 @@ sap.ui.define([
             if (!this.byId("helloDialog")) {
                 Fragment.load({
                     id: oView.getId(),
-                    name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                    controller: this
                 }).then(function (oDialog){
                     oView.addDependent(oDialog);
                     oDialog.open();
@@ -27,6 +28,9 @@ sap.ui.define([
             } else{
                 this.byId("helloDialog").open();
             }
+        },
+        onCloseDialog: function () {
+            this.byId("helloDialog").close();
         }
     })
 });
